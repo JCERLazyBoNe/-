1fe2e746f0ee57e62fbcd880649efee819328920
JCERLazyBoNe <115054960+JCERLazyBoNe@users.noreply.github.com>
2022-10-05 16:13:19 +0800

    Add files via upload

--- ik.cpp
+++ ik.cpp
@@ -5,12 +5,13 @@ using namespace std;
 char p[56]={'.','*','#','1','W'};
 char k[14][14];
 int x=7,y=7;
+int f=0;
 int xa=8,ya=8;
 void check(int a,int b)
 {
 	if(k[a][b]=='.')
 	cout<<"一条土路"<<endl;
-	else if(k[a][b]=='#')
+	else if(k[a][b]=='K')
 	cout<<"一面坚硬的墙"<<endl;
 	else if(k[a][b]=='*')
 	cout<<"人为建造的障碍物"<<endl;
@@ -21,18 +22,19 @@ int main()
 	for(int l=0;l<=14;l++)
 	k[i][l]='.';
 	for(int i=0;i<=14;i++)
-	k[i][0]=k[i][14]=k[14][i]=k[0][i]='#';
+	k[i][0]=k[i][14]=k[14][i]=k[0][i]='K';
 	string a;
 	k[x][y]='1';
 	k[xa][ya]='2';
-	for(int i=0;i<=14;i++)
+	for(int i=1;i<=13;i++)
 	{
-		for(int l=0;l<=14;l++)
+		for(int l=1;l<=13;l++)
 		cout<<k[i][l]<<' ';
 		cout<<endl;
 	}
 		cout<<"玩家1 wasd移动 k查看 f放置 p破坏\n";
 		cout<<"玩家2 8456移动\n";
+	f=0;
 	while(1)
 	{
 		char c=_getch();
@@ -49,14 +51,84 @@ int main()
 			k[xa][ya]='.',ya++;
 			k[xa][ya]='2';
 			
-			for(int i=0;i<=14;i++)
+			for(int i=1;i<=13;i++)
 			{
-				for(int l=0;l<=14;l++)
+				for(int l=1;l<=13;l++)
 				cout<<k[i][l]<<' ';
 				cout<<endl;
 			}
-			cout<<"wasd移动 k查看 f放置 p破坏\n";
+			cout<<"玩家1 wasd移动 k查看 f放置 p破坏\n";
 			cout<<"玩家2 8456移动\n";
+			continue;
+		}
+		if(f!=0&&(c<'0'||c>'9'))
+		{
+//			cout<<f<<' '<<(f!=0)<<endl;
+			if(f==1&&(c<'0'||c>'9'))
+			{
+				f=0;
+				if(c=='w'&&k[x-1][y]=='.')
+				k[x-1][y]='*';
+				else if(c=='s'&&k[x+1][y]=='.')
+				k[x+1][y]='*';
+				else if(c=='a'&&k[x][y-1]=='.')
+				k[x][y-1]='*';
+				else if(c=='d'&&k[x][y+1]=='.')
+				k[x][y+1]='*';
+				for(int i=1;i<=13;i++)
+				{
+					for(int l=1;l<=13;l++)
+					cout<<k[i][l]<<' ';
+					cout<<endl;
+				}
+				cout<<"玩家1 wasd移动 k查看 f放置 p破坏\n";
+				cout<<"玩家2 8456移动\n";
+				continue;
+			}
+			if(f==2&&(c<'0'||c>'9'))
+			{
+				f=0;
+				if(c=='w'&&k[x-1][y]=='*')
+				k[x-1][y]='.';
+				else if(c=='s'&&k[x+1][y]=='*')
+				k[x+1][y]='.';
+				else if(c=='a'&&k[x][y-1]=='*')
+				k[x][y-1]='.';
+				else if(c=='d'&&k[x][y+1]=='*')
+				k[x][y+1]='.';
+				f=0;
+				for(int i=1;i<=13;i++)
+				{
+					for(int l=1;l<=13;l++)
+					cout<<k[i][l]<<' ';
+					cout<<endl;
+				}
+				cout<<"玩家1 wasd移动 k查看 f放置 p破坏\n";
+				cout<<"玩家2 8456移动\n";
+				continue;
+			}
+			if(f==3&&(c<'0'||c>'9'))
+			{
+				f=0;
+				for(int i=1;i<=13;i++)
+				{
+					for(int l=1;l<=13;l++)
+					cout<<k[i][l]<<' ';
+					cout<<endl;
+				}
+				cout<<"玩家1 wasd移动 k查看 f放置 p破坏\n";
+				cout<<"玩家2 8456移动\n";
+				if(c=='w')
+				check(x-1,y);
+				else if(c=='s')
+				check(x+1,y);
+				else if(c=='a')
+				check(x,y-1);
+				else if(c=='d')
+				check(x,y+1);
+				f=0;
+				f=0;
+			}
 			continue;
 		}
 		if(c=='w'&&k[x-1][y]=='.')
@@ -68,51 +140,29 @@ int main()
 		else if(c=='d'&&k[x][y+1]=='.')
 		k[x][y]='.',y++;
 		k[x][y]='1';
-		for(int i=0;i<=14;i++)
+		for(int i=1;i<=13;i++)
 		{
-			for(int l=0;l<=14;l++)
+			for(int l=1;l<=13;l++)
 			cout<<k[i][l]<<' ';
 			cout<<endl;
 		}
-		cout<<"wasd移动 k查看 f放置 p破坏\n";
+		cout<<"玩家1 wasd移动 k查看 f放置 p破坏\n";
 		cout<<"玩家2 8456移动\n";
+		f=0;
 		if(c=='k')
 		{
-			c=_getch();
-			if(c=='w')
-			check(x-1,y);
-			else if(c=='s')
-			check(x+1,y);
-			else if(c=='a')
-			check(x,y-1);
-			else if(c=='d')
-			check(x,y+1);
+			f=3;
 			continue;
 		}
 		else if(c=='f')
 		{
-			c=_getch();
-			if(c=='w')
-			k[x-1][y]='*';
-			else if(c=='s')
-			k[x+1][y]='*';
-			else if(c=='a')
-			k[x][y-1]='*';
-			else if(c=='d')
-			k[x][y+1]='*';
+			f=1;
 			continue;
 		}
 		else if(c=='p')
 		{
-			c=_getch();
-			if(c=='w'&&k[x-1][y]=='*')
-			k[x-1][y]='.';
-			else if(c=='s'&&k[x+1][y]=='*')
-			k[x+1][y]='.';
-			else if(c=='a'&&k[x][y-1]=='*')
-			k[x][y-1]='.';
-			else if(c=='d'&&k[x][y+1]=='*')
-			k[x][y+1]='.';
+			f=2;
+			continue;
 		}
 	}
 	return 0;
